989d2d1b72fc2ae7ca709707a8d9501eff09e353
Yosiak-alv <Josias_romero60@outlook.com>
2023-09-14 11:44:29 -0600

    Update Login.js
    Co-Authored-By: Cotto213 <61715791+Cotto213@users.noreply.github.com>
    Co-Authored-By: Luis Bonilla <124472850+bocchiko@users.noreply.github.com>

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,13 +1,47 @@
-import { View, Text, TextInput, Pressable,Image } from 'react-native';
-import React from 'react';
-
+import { View, Text, TextInput, Pressable,Image,ActivityIndicator } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { Messages } from '../components/Messages';
 import { GuestLayout } from '../layouts/GuestLayout';
 import { PrimaryButton } from '../components/PrimaryButton';
 import {TxtInput }from '../components/TxtInput'
 import { Card } from '../components/Card';
+import { loginAttempt } from '../hooks/AuthApi';
+import { useMutation } from 'react-query';
 
-export const Login = () => {
 
+export const Login = ({navigation}) => {
+
+    const [email,setEmail] = useState('');
+    const [password,setPassword] = useState('');
+    const [success,setSuccess] = useState([]);
+    const [generalException,setGeneralException] = useState([]);
+    
+    const UserLogin = useMutation({
+        mutationFn: loginAttempt,
+        
+        onError: (error,variables,context) => {
+            console.log(error);
+            setGeneralException(error?.response?.data ? false : true);
+
+        },
+        onSuccess: (data, variables, context) => {
+            setSuccess(data.data);
+            console.log(data.data);
+        },
+    })
+
+    const handleLogin = () => {
+        if(email.trim().length ==0 || password.trim().length == 0){
+            console.log('No Null values !!!');
+        }else{
+            const user = {
+                email:email,
+                password:password
+            }   
+            UserLogin.mutate(user);
+        }
+        
+    }
     return (
         <GuestLayout>
             <View className="items-center justify-center">
@@ -21,8 +55,8 @@ export const Login = () => {
 
             <Text className="text-5xl font-bold mb-6 text-gray-200 mt-5">Sign In</Text>
             {/* form */}
-            <TxtInput placeholder="Enter your Email"/> 
-            <TxtInput placeholder="Enter your password" passEntry={true}/> 
+            <TxtInput onChangeText={(text) => setEmail(text)} placeholder="Enter your Email"/> 
+            <TxtInput onChangeText={(text) => setPassword(text)}  placeholder="Enter your password" passEntry={true}/> 
             
             <View className="flex flex-row justify-between items-center mt-3" >
                 <View className="flex-row items-center">
@@ -38,13 +72,33 @@ export const Login = () => {
                     </Text>
                 </Pressable>
             </View>
-            {/* buttom*/}
+
             <View className="flex flex-row justify-end mt-5">         
-               <PrimaryButton message='log in'/>
+               <PrimaryButton onPress={() => {handleLogin()}} 
+                message='log in'/>
             </View>
-           {/*  <Card>
-                HOLA
-            </Card> */}
+
+            {   UserLogin.isLoading ? (
+                    <ActivityIndicator size="large" style={{marginVertical:16}} color="white"/>
+                ) : (
+                    <View>
+                        {UserLogin.isError ? (
+                            generalException ? (
+                                <Messages message={`Here was a problem processing Login : ${UserLogin.error}`} level={'error'}/>
+                            ):(
+                                <Messages message={`${UserLogin.error.response.data?.message } \n ${UserLogin.error.response.data?.error?.email ? UserLogin.error.response.data?.error?.email : ''} \n ${UserLogin.error.response.data?.error?.password ? UserLogin.error.response.data?.error?.password : ''}`} 
+                                    level={'error'}
+                                /> 
+                            )    
+                        ) : null}
+
+                        {UserLogin.isSuccess ? navigation.navigate('Home',{
+                            screen: 'HomePage',
+                            params: { token: success.token },
+                        }) : null}
+                    </View>
+                )
+            } 
         </GuestLayout>
     );
 }
