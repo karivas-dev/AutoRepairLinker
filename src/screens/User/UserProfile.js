c3b94179b2dc95aa9bb25ae3eb437529fba4ce40
Yosiak-alv <Josias_romero60@outlook.com>
2023-09-15 15:05:35 -0600

    Update UserProfile.js

--- src/screens/User/UserProfile.js
+++ src/screens/User/UserProfile.js
@@ -1,56 +1,95 @@
-import { View, Text,Image} from 'react-native';
+import { View, Text,Image, ActivityIndicator} from 'react-native';
 import { AuthenticateLayout } from '../../layouts/AuthenticateLayout';
 import { Feather } from '@expo/vector-icons';
 import { PrimaryButton } from '../../components/PrimaryButton';
 import {SecondaryButton }from '../../components/SecondaryButton'
+import {DangerButton} from '../../components/DangerButton';
 import { Card } from '../../components/Card';
 import { Header } from '../../components/Header';
+import { Messages } from '../../components/Messages';
+
+import { useMutation } from 'react-query';
+import { logoutAttempt } from '../../hooks/AuthApi';
+
+import { logout ,getAuthToken} from '../../context/AuthContext';
 
 export const UserProfile = ({navigation}) => {
 
-    return (
-        
-        <AuthenticateLayout>
-        <Header navigation={navigation}/>
-        <View className="flex flex-1 flex-col justify-center items-center" >
-           <View className="flex-none w-full max-w-sm" >
-                <Card>
-                    <View className="flex flex-row justify-between">
-                        <View className="py-2">
-                            <Feather name="user" size={62} color="#F1F6F5" />
-                        </View>
-                        <View>
-                           <View>
-                                <PrimaryButton message='Edit'/>
-                           </View>
-                           <View className="mt-2">
-                                <SecondaryButton message="Add Car" />
-                           </View>
-                        </View>
-                    </View>
-                </Card>
-           </View>
-            <View className="w-full max-w-sm">
-                <Card >
-                    <Text className="text-gray-200 text-lg font-bold text-center" >User.Name, User.Lastname</Text><Text>{`\n`}</Text>
-                    <Text className="text-gray-200 text-lg text-center">User.Email</Text><Text>{`\n`}</Text>
+    const userLogOut = useMutation({
+        mutationFn:logoutAttempt,
 
-                    <Text className="text-gray-200 text-lg text-center" > 
-                        <Text className="text-gray-200 text-lg font-bold" >Phone: </Text> User.Phone
-                    </Text><Text>{`\n`}</Text>
-                    <Text className="text-gray-200 text-lg text-center" > 
-                        <Text className="text-gray-200 text-lg font-bold" >District: </Text> User.District
-                    </Text><Text>{`\n`}</Text>
-                </Card>
-            </View>
-            <View className="flex-none w-full max-w-sm">
-                <Card>
-                    <Text className="text-gray-200 text-lg font-extrabold " >User.Area</Text>
-                </Card>
-            </View>
+        onError: (error,variables,context) => {
+            console.log(error);
+        },
+        onSuccess: (data, variables, context) => {
+            logout();
+            console.log(data);
+            navigation.navigate('Login');
+        },
+        
+    });
+
+    const handleLogOut = async() => {
+        if(confirm('You want to Log Out ??? ..')){
+            await userLogOut.mutate();
+        }
+    }
+
+    return (
+        <AuthenticateLayout>
+            <View className="flex flex-1 flex-col justify-center items-center" >
+                {/* Log Out Handling */}
+                <View className="w-full max-w-sm">
+                    {   userLogOut.isLoading ? (
+                            <ActivityIndicator size="large" style={{marginVertical:16}} color="white"/>
+                        ) : (
+                            <View>
+                                {userLogOut.isError ? (
+                                    <Messages message={`Here was a problem processing Logout : ${userLogOut.error}`} level={'error'}/>
+                                ) : null}
+
+                              {/*   {userLogOut.isSuccess ? navigation.navigate('Login') : null} */}
+                            </View>
+                        )
+                    } 
+                </View>
             
-        </View>
-      
-   </AuthenticateLayout>
+                <View className="flex-none w-full max-w-sm" >
+                    <Card>
+                        <View className="flex flex-row justify-between">
+                            <View className="py-2">
+                                <Feather name="user" size={62} color="#F1F6F5" />
+                            </View>
+                            <View>
+                            <View>
+                                <PrimaryButton message='Edit'/>
+                            </View>
+                            <View className="mt-2">
+                                <DangerButton onPress={() => handleLogOut()} message="Log Out" />    
+                            </View>
+                            </View>
+                        </View>
+                    </Card>
+                </View>
+                <View className="w-full max-w-sm">
+                    <Card >
+                        <Text className="text-gray-200 text-lg font-bold text-center" >User.Name, User.Lastname</Text><Text>{`\n`}</Text>
+                        <Text className="text-gray-200 text-lg text-center">User.Email</Text><Text>{`\n`}</Text>
+
+                        <Text className="text-gray-200 text-lg text-center" > 
+                            <Text className="text-gray-200 text-lg font-bold" >Phone: </Text> User.Phone
+                        </Text><Text>{`\n`}</Text>
+                        <Text className="text-gray-200 text-lg text-center" > 
+                            <Text className="text-gray-200 text-lg font-bold" >District: </Text> User.District
+                        </Text><Text>{`\n`}</Text>
+                    </Card>
+                </View>
+                <View className="flex-none w-full max-w-sm">
+                    <Card>
+                        <Text className="text-gray-200 text-lg font-extrabold " >User.Area</Text>
+                    </Card>
+                </View>
+            </View> 
+        </AuthenticateLayout>
     )
 }
