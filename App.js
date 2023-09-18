c210538a13a663726ccdc45bf51ed018e3b80605
Yosiak-alv <Josias_romero60@outlook.com>
2023-09-17 18:50:25 -0600

    Update App.js
    Co-Authored-By: Luis Bonilla <124472850+bocchiko@users.noreply.github.com>
    Co-Authored-By: Cotto213 <61715791+Cotto213@users.noreply.github.com>

--- App.js
+++ App.js
@@ -1,15 +1,9 @@
 import React,{useEffect, useState} from 'react';
 import "./styles.css";
-import { useNavigation } from '@react-navigation/native';
-import { Login } from './src/screens/Login';
-import { FormOwner } from './src/screens/Owners/FormOwner';
-import {HomePage} from "./src/screens/HomePage";
-import { DetailOwner } from './src/screens/Owners/DetailOwner';
-import { UserProfile } from './src/screens/User/UserProfile';
-import { ResetPassword } from './src/screens/ResetPassword';
-/* //import { Header } from './src/components/Header'; */
+
 import { NavigationContainer } from '@react-navigation/native';
-import HomeStackNavigator from './src/Navigations/Navigator';
+import { HomeStackNavigator } from './src/Navigations/Navigator';
+import { LoginStackNavigator } from './src/Navigations/Navigator';
 import { getAuthToken } from './src/context/AuthContext';
 
 import { QueryClient, QueryClientProvider } from 'react-query';
@@ -29,9 +23,9 @@ export default function App() {
       // Si existe un token, el usuario ya ha iniciado sesión, navega al flujo principal
       if (token) {
         setHaveToken(true);
+        console.log('aqui')
       } else {
         setHaveToken(false);
-        //navigation.navigate('Login')
       }
     } catch (error) {
       console.error('Error al verificar el estado de inicio de sesión: ', error);
@@ -41,19 +35,16 @@ export default function App() {
   return (
     <QueryClientProvider client={queryClient}>
       {
-       haveToken ? (
+        haveToken ? (
           <NavigationContainer>
-              <HomeStackNavigator></HomeStackNavigator>
+            <HomeStackNavigator></HomeStackNavigator>
           </NavigationContainer>
-         
         ):(
           <NavigationContainer>
-             <Login/>
+            <LoginStackNavigator></LoginStackNavigator>
           </NavigationContainer>
         )
       }
     </QueryClientProvider>
-  )
- 
-  
+  );
 }
