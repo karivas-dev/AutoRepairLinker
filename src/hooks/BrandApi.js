35f7d54d75b1e11d6146eb01411f14658a160ec9
Haruki1707 <72423267+Haruki1707@users.noreply.github.com>
2023-09-19 23:34:53 -0600

    Updated BrandApi to work with route wrapper

--- src/hooks/BrandApi.js
+++ src/hooks/BrandApi.js
@@ -1,32 +1,19 @@
-import axios from "axios";
-import { useQuery,useInfiniteQuery } from "react-query";
-import { getAuthToken } from "../context/AuthContext";
-
-
-const brandApi = axios.create({baseURL: 'http://127.0.0.1:8000/api/v1'})
-
-brandApi.interceptors.request.use(async (config) => {
-    const token = await getAuthToken();
-    if (token){
-        config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-});
+import axiosRoute from "../utils/route";
 
 const fetchBrands = async (page) => {
-    const res = await brandApi.get(`/brands?page=${page}` );
+    const res = await axiosRoute.get('brands.index', {page: page});
     console.log(res.data);
     return res.data;
 }
 
 const fetchOneBrand = async (id) => {
-    const res = await brandApi.get(`/brands/${id}`);
+    const res = await axiosRoute.get('brands.show', {brand: id})
     console.log(res.data);
     return res.data;
 }
 
-const storeBrand = (brand) => (brandApi.post('/brands',brand));
+const storeBrand = (brand) => (axiosRoute.post('brands.store', null, brand));
 
-const updateBrand = (brand) => (brandApi.put(`/brands/${brand.id}`, brand));
+const updateBrand = (brand) => (axiosRoute.put('brands.update', brand.id, brand));
 
-export {fetchBrands , fetchOneBrand , storeBrand , updateBrand};
+export {fetchBrands, fetchOneBrand, storeBrand, updateBrand};
