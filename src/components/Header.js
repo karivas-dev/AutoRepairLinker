89dcb585d7b8293b8da68fdf848f89f6a89d1d9d
Yosiak-alv <Josias_romero60@outlook.com>
2023-10-04 13:34:26 -0600

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,15 @@
 import { View, Text } from 'react-native'
 import React from 'react';
-import { TouchableOpacity } from 'react-native';
+import { Pressable } from 'react-native';
 import { AntDesign } from '@expo/vector-icons';
 
 export const Header = ({navigation}) => {
     return(
         <View className="flex flex-row justify-start mt-5">   
                  
-            <TouchableOpacity onPress={ () => navigation.goBack()}>
+            <Pressable onPress={ () => navigation.goBack()}>
                 <AntDesign name="arrowleft" size={40} color="white" />
-            </TouchableOpacity>
+            </Pressable>
         
         </View>
     )
